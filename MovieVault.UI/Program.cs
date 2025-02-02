f4ce58eb8433585d7657350bf738579055a8813a
Noris Benseghir <n0r1s@live.be>
2025-02-03 00:41:16 +0100

    Add logging system and going full async

--- MovieVault.UI/Program.cs
+++ MovieVault.UI/Program.cs
@@ -9,28 +9,12 @@ namespace MovieVault.UI
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static async Task Main()
         {
-            AllocConsole();
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            try
-            {
-                using (SqlConnection conn = DatabaseManager.GetConnection())
-                {
-                    conn.Open();
-                    Console.WriteLine("Connection to the database successful!");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Connection error: " + ex.Message);
-            }
             Application.Run(new Form1());
         }
-
-        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
-        private static extern bool AllocConsole();
     }
 }
